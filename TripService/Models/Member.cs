90865a0007f0b5b9abbace28061b4ef94381bcab
Joshna <joshnapogala@gmail.com>
2026-01-05 05:42:45 -0800

    TripService - Add Model for Member & Trip

---
+++ TripService/Models/Member.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TripService.Models
+{
+    public class Member
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int ExternalRefId { get; set; }
+
+        [Required]
+        public string MemberId { get; set; } = string.Empty;
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        // Navigation property for related Trips
+        // one member can have many trips
+        public ICollection<Trip> Trips { get; set; } = new List<Trip>();
+    }
+}
---
+++ TripService/Models/Trip.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TripService.Models
+{
+    public class Trip
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime TripDate { get; set; }
+
+        [Required]
+        public string PickUp { get; set; } = string.Empty;
+
+        [Required]
+        public string Destination { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Cost { get; set; }
+
+        //lyft uber etc
+        [Required]
+        public string Vehicle { get; set; } = string.Empty;
+
+        // Foreign key to Member
+        [Required]
+        public int MemberId { get; set; }
+
+        // Navigation property
+        public Member Member { get; set; } = null!;
+    }
+}
